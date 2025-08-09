24eec6fdc53154ed97db620f4b592d618a3951af
Ryan Stafford <ryan@nanorex.net>
2025-08-08 20:34:32 -0400

    Update mvt.js

--- mvt.js
+++ mvt.js
@@ -1,17 +1,17 @@
-const MVT = (table) => {
+const MVT = table => {
   const inputs = Array.from(table.querySelectorAll('thead input')),
         tbody = table.querySelector('tbody'),
-        rows = Array.from(tbody.querySelectorAll('tr'))
+        rows = Array.from(tbody.querySelectorAll('tr'));
   Array.from(table.querySelectorAll('th'))
     .forEach((header, i, headers) => {
       (header.querySelector('input') || {}).oninput = () => rows
           .forEach(row => row.style.display = inputs
             .every(input => !input.value ||
               (new RegExp(input.value,'i')).test(row.children[i].innerHTML))
-            ? '' : 'none')
+            ? '' : 'none');
       header.onclick = (e) => {
         if (e.target.tagName == 'INPUT' || !e.target.innerHTML) return;
-        headers.filter(h => h != header).forEach(h => h.dataset.sort = '')
+        headers.filter(h => h != header).forEach(h => h.dataset.sort = '');
         const sort = e.target.dataset.sort = (e.target.dataset.sort == '-1') ? 1 : -1;
         rows.sort((a, b) => [a, b]
             .map(row => row.querySelectorAll('td')[i])
@@ -19,7 +19,7 @@ const MVT = (table) => {
               .find((v, i) => !isNaN(v) || i))
             .map(v => isNaN(v) ? v : parseInt(v))
             .reduce((x, y) => (x < y) ? sort : ((x > y) ? -sort : 0)), 0)
-          .forEach(row => tbody.appendChild(row))
+          .forEach(row => tbody.appendChild(row));
       }
-  })
+  });
 }
